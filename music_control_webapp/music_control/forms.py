0461f2e902fe2de32d202e91d66b684ec4e7f4e2
jameschuang2002 <84842755+jameschuang2002@users.noreply.github.com>
2022-03-25 08:44:06 -0700

    minor edits

--- music_control_webapp/music_control/forms.py
+++ music_control_webapp/music_control/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class StringInputForm(forms.Form):
-    input = forms.CharField(label = "input", max_length=1000)
+    input = forms.CharField(label = "input", max_length=1000, initial="John")
--- music_control_webapp/music_control/views.py
+++ music_control_webapp/music_control/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .forms import StringInputForm
-from django.template import RequestContext
 
 # Create your views here.
 def index(request):
