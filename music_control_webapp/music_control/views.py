5ed85f4ec144d6c15f8c881a452d442ddc80d84d
jameschuang2002 <84842755+jameschuang2002@users.noreply.github.com>
2022-03-25 09:41:37 -0700

    get_value

--- music_control_webapp/music_control/views.py
+++ music_control_webapp/music_control/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .forms import StringInputForm
+from django.http import HttpResponse
 
+string_input = ""
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
@@ -9,7 +11,12 @@ def arduino_button(request):
     if request.method == 'POST':
         form = StringInputForm(request.POST)
         if(form.is_valid()):
-            return form.cleaned_data['input']
+            string_input = form.cleaned_data['input']
+            return HttpResponse(string_input)
     else:
         form = StringInputForm()
         return render(request, 'arduino/button.html', {'form': form})
+
+def get_value():
+    return string_input
+    
