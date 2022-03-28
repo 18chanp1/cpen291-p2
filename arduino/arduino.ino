23380d678d9cd9f61a5caa196fd18a6169f35a75
Vanessa Quon <77385101+van1011@users.noreply.github.com>
2022-03-28 14:40:21 -0700

    Update arduino.ino

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -13,11 +13,42 @@
   while (Serial.available()) { // If data is available to read,
     val = Serial.read(); // read it and store it in val
   }
-  if (val == 'H') { // If H was received
-    digitalWrite(ledPin, HIGH); // turn the LED on
-  } else {
-    digitalWrite(ledPin, LOW); // Otherwise turn it OFF
-  }
+  Serial.print(val);
+
+  //return string with play mod 0/1 | button pressed 01-10 | text box answer
+//  if (val[0] == "type") { // If text box mode
+//    parse(val);
+//    digitalWrite(ledPin, HIGH); // turn the LED on
+//  } else {
+//    digitalWrite(ledPin, LOW); // Otherwise turn it OFF
+//  }
   delay(100); // Wait 100 milliseconds for next reading
+ }
+
+ void parse(String val){
+
+  bool readNote = 1;
+  char note;
+  int playTime;
+  
+  for (auto it = val.begin() + 3; it < val.end(); it++) { 
+    //skip first three letters play mode, button
+    
+    if (note == 1 && isalpha(*it)){ //enter note
+      note = *it;
+    } else if (note == -1 && isdigit(*it)){
+      playTime = int(*it);
+    } else {
+      //print error of some sort
+    }
+
+    if(readNote == -1){
+      //play note 
+      delay(playTime*1000); //play for playTime seconds
+    }
+    
+    note *= -1;
+  }
+    
  }
  
