79307720b8a175be4d402327c15cfacde05e809a
jameschuang2002 <84842755+jameschuang2002@users.noreply.github.com>
2022-03-28 21:12:00 -0700

    website->arduino communication

--- main_server/main_server/views.py
+++ main_server/main_server/views.py
@@ -1,7 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import MusicNotes
+
 def test(request):
     return HttpResponse("Hello World")
 
 def index(request):
     return render(request, template_name='index.html')
+
+def input(request):
+    if request.method == 'POST':
+        music_input = MusicNotes(notes=request.POST['arguments'])
+        if music_input:
+            music_input.save()
+    elif request.method == 'GET':
+        return HttpResponse(MusicNotes.objects.last().notes)
+
