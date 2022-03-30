486a6807c44b71c66d1ab6cb9f7b3d6cf3f33953
jameschuang2002 <84842755+jameschuang2002@users.noreply.github.com>
2022-03-29 19:52:53 -0700

    minor edits

--- main_server/main_server/views.py
+++ main_server/main_server/views.py
@@ -29,6 +29,6 @@ def status(request):
         return HttpResponse("YAY")
     elif request.method == 'GET':
         if StatusMessage.msg == 'playing':
-            return render(request, template_name = 'index.html') #TODO: should change to related playing page here
+            return HttpResponse("PLAYING") #TODO: should change to related playing page here
         elif StatusMessage.msg == 'free':
             return render(request, template_name = 'index.html')
